05a75196513a849f57337163fe8bbc5383627f64
ChemaKing13 <jm2000ho@gmail.com>
2023-04-15 22:39:13 -0600

    if statement added

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,41 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-var currentDayEl = document.querySelector("#currentDay"); 
+var currentDayEl = $('#currentDay'); 
+
+//here we add the specific classes to the hourBox element by using JQuery
+var hourBox = $('.hour-box').addClass('hour-box time-block row'); 
+
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 function displayTime() {
-  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a'); 
+  var rightNow = dayjs().format('dddd, MMMM D'); 
   currentDayEl.text(rightNow); 
 }
 
+//get the current hour and stored in a new varible 
+var currentHour = dayjs().format('h'); 
+
+$('.hour-box').each(function() {
+  var hourBox = $(this); 
+
+  var boxId = parseInt(hourBox.attr('id'));
+
+  //console.log(boxId); 
+
+
+
+})
+
+
+
+
+
+
+
+
+
 
 $(function () {
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -25,7 +52,14 @@ $(function () {
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+    
+    
   });
+
+
+
+  displayTime(); 
+
+
+
   
